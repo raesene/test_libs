3b31a54208aa39c17402cb518e14a4e52ca87880
Rory McCune <rorym@mccune.org.uk>
2015-04-15 14:59:03 +0100

    Updated rack server and javascript phone home

--- rack/alien_server.rb
+++ rack/alien_server.rb
@@ -3,6 +3,7 @@ require 'rack'
 require 'logger'
 require 'date'
 require 'thin'
+require 'cgi/util'
 builder = Rack::Builder.new do
   @@logger = Logger.new('alien.log')
   @@ets = Array.new
@@ -10,10 +11,21 @@ builder = Rack::Builder.new do
     run Proc.new {|env| [200, {"Content-Type" => "text/html"}, ["<h1> Rack Pen Test Helper</h1>","<h2>Part of a project on lib security, see https://github.com/raesene/test_libs for more info </h2>"]]}
   end
  
-  map '/et_no_way_you_got_here_randomly' do
+  map '/javascript_no_way_you_got_here_randomly' do
     app = proc do |env|
       req = Rack::Request.new(env) 
-      info = req.ip.to_s + ", " + DateTime.now.to_s + ',' + req.user_agent
+      info = 'JavaScript, ' + req.ip.to_s + ", " + DateTime.now.to_s + ',' + req.user_agent
+      @@logger.info(info)
+      @@ets << info
+      [200, {"Content-Type" => "text/html"}, ["you got a response"]]
+    end
+    run app
+  end
+
+  map '/ruby_no_way_you_got_here_randomly' do
+    app = proc do |env|
+      req = Rack::Request.new(env) 
+      info = 'Ruby, ' + req.ip.to_s + ", " + DateTime.now.to_s + ',' + req.user_agent
       @@logger.info(info)
       @@ets << info
       [200, {"Content-Type" => "text/html"}, ["you got a response"]]
@@ -27,7 +39,7 @@ builder = Rack::Builder.new do
       if @@ets.length > 0
         out << "<H1>Got #{@@ets.length.to_s} callers so far</H1>"
         @@ets.each do |et|
-          out << et + "<br />"
+          out << CGI.escapeHTML(et) + "<br />"
         end
       else
         out << "Nothing Grabbed so far"
